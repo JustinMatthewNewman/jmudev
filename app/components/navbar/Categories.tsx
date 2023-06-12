04808fa1fdf90a2f59932ddb3bee89db79440e9b
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-06-12 19:33:11 -0400

    Error resolved.. was the anywhere option for location the whole time..

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,109 +1,111 @@
-'use client';
+"use client";
 
-import { usePathname, useSearchParams } from 'next/navigation';
-import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
-import { 
-  GiBarn, 
-  GiBoatFishing, 
-  GiCactus, 
-  GiCastle, 
-  GiCaveEntrance, 
-  GiForestCamp, 
-  GiIsland,
-  GiWindmill
-} from 'react-icons/gi';
-import { FaSkiing } from 'react-icons/fa';
-import { BsSnow } from 'react-icons/bs';
-import { IoDiamond } from 'react-icons/io5';
-import { MdOutlineVilla } from 'react-icons/md';
+import { usePathname, useSearchParams } from "next/navigation";
+
+import {
+  HiCode,
+  HiChartPie,
+  HiTranslate,
+  HiPhotograph,
+  HiMusicNote,
+  HiUserGroup,
+  HiTruck,
+  HiPaperAirplane,
+  HiPencil,
+  HiInboxIn,
+  HiPhoneIncoming,
+  HiTemplate,
+  HiAcademicCap,
+  HiEyeOff,
+  HiPresentationChartLine,
+} from "react-icons/hi";
 
 import CategoryBox from "../CategoryBox";
-import Container from '../Container';
-
+import Container from "../Container";
 
 export const categories = [
   {
-    label: 'Beach',
-    icon: TbBeach,
-    description: 'This property is close to the beach!',
+    label: "Programming",
+    icon: HiCode,
+    description: "This property is programming!",
   },
   {
-    label: 'Windmills',
-    icon: GiWindmill,
-    description: 'This property is has windmills!',
+    label: "Math",
+    icon: HiChartPie,
+    description: "This property is has Math!",
   },
   {
-    label: 'Modern',
-    icon: MdOutlineVilla,
-    description: 'This property is modern!'
+    label: "Languages",
+    icon: HiTranslate,
+    description: "This property is Langs!",
   },
   {
-    label: 'Countryside',
-    icon: TbMountain,
-    description: 'This property is in the countryside!'
+    label: "Art",
+    icon: HiPhotograph,
+    description: "This property is art!",
   },
   {
-    label: 'Pools',
-    icon: TbPool,
-    description: 'This is property has a beautiful pool!'
+    label: "Music",
+    icon: HiMusicNote,
+    description: "This is property has a beautful song!",
   },
   {
-    label: 'Islands',
-    icon: GiIsland,
-    description: 'This property is on an island!'
+    label: "Sports",
+    icon: HiUserGroup,
+    description: "This property is on sports!",
   },
   {
-    label: 'Lake',
-    icon: GiBoatFishing,
-    description: 'This property is near a lake!'
+    label: "Skill/Trade",
+    icon: HiTruck,
+    description: "This property is near a skill!",
   },
   {
-    label: 'Skiing',
-    icon: FaSkiing,
-    description: 'This property has skiing activies!'
+    label: "Travel",
+    icon: HiPaperAirplane,
+    description: "This property has Travel!",
   },
   {
-    label: 'Castles',
-    icon: GiCastle,
-    description: 'This property is an ancient castle!'
+    label: "Writing",
+    icon: HiPencil,
+    description: "This property is an ancient castle!",
   },
   {
-    label: 'Caves',
-    icon: GiCaveEntrance,
-    description: 'This property is in a spooky cave!'
+    label: "Resume",
+    icon: HiInboxIn,
+    description: "This property is resume workshops!",
   },
   {
-    label: 'Camping',
-    icon: GiForestCamp,
-    description: 'This property offers camping activities!'
+    label: "Interviews",
+    icon: HiPhoneIncoming,
+    description: "This property offers activities!",
   },
   {
-    label: 'Arctic',
-    icon: BsSnow,
-    description: 'This property is in arctic environment!'
+    label: "WebDev",
+    icon: HiTemplate,
+    description: "This property is in arctic environment!",
   },
   {
-    label: 'Desert',
-    icon: GiCactus,
-    description: 'This property is in the desert!'
+    label: "Courses",
+    icon: HiAcademicCap,
+    description: "This property is in the school!",
   },
   {
-    label: 'Barns',
-    icon: GiBarn,
-    description: 'This property is in a barn!'
+    label: "Personal",
+    icon: HiEyeOff,
+    description: "This property is personal!",
   },
   {
-    label: 'Lux',
-    icon: IoDiamond,
-    description: 'This property is brand new and luxurious!'
-  }
-]
+    label: "Speaking",
+    icon: HiPresentationChartLine,
+    description: "This property is brand new and public speaking!",
+  },
+];
 
 const Categories = () => {
   const params = useSearchParams();
-  const category = params?.get('category');
+  const category = params?.get("category");
   const pathname = usePathname();
-  const isMainPage = pathname === '/';
+  const isMainPage = pathname === "/";
 
   if (!isMainPage) {
     return null;
@@ -122,7 +124,7 @@ const Categories = () => {
         "
       >
         {categories.map((item) => (
-          <CategoryBox 
+          <CategoryBox
             key={item.label}
             label={item.label}
             icon={item.icon}
@@ -132,6 +134,6 @@ const Categories = () => {
       </div>
     </Container>
   );
-}
- 
+};
+
 export default Categories;
