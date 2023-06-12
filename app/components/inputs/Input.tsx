a6efd550a0710743b055c5fd5a9ec3edd4e81ae9
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-06-12 17:24:19 -0400

    Colors

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -61,8 +61,8 @@ const Input: React.FC<InputProps> = ({
           disabled:opacity-70
           disabled:cursor-not-allowed
           ${formatPrice ? 'pl-9' : 'pl-4'}
-          ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-          ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+          ${errors[id] ? 'border-slate-500' : 'border-neutral-300'}
+          ${errors[id] ? 'focus:border-slate-500' : 'focus:border-black'}
         `}
       />
       <label 
